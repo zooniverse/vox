28f5f80237f3215105a5c0fda33e33bb448c0ea3
Roger Hutchings <roger@hutchin.gs>
2016-07-10 20:54:10 +0100

    Add stage 0 preset for babel

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -53,6 +53,13 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel',
+        query: {
+          presets: [
+            'es2015',
+            'react',
+            'stage-0',
+          ]
+        }
       },
       {
         test: /\.json$/,
