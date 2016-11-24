8329cb181be49617e3b7c9ea2c578b2877c01fbd
Simone Duca <simone@zooniverse.org>
2016-11-24 17:14:26 +0000

    Update to more secure port

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ if (!isProduction) {
   });
 }
 
-app.listen(port, '0.0.0.0', function onStart(err) {
+app.listen(port, '127.0.0.1', function onStart(err) {
   if (err) {
     console.log(err);
   }
 
-  console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  console.info('==> Listening on port %s. Open up http://127.0.0.1:%s/ in your browser.', port, port);
 });
