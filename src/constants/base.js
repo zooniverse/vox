714659ea3ff79934b8ecb7da92db85f574081bfe
Simone Duca <simone@zooniverse.org>
2016-11-29 16:24:55 +0000

    Remove trailing comma.
    Update redirect url to production.
    Remove gtm template.

--- src/constants/base.js
+++ src/constants/base.js
@@ -3,7 +3,7 @@ const base = {
   authDomain: 'project-6243802502502885389.firebaseapp.com',
   databaseURL: 'https://project-6243802502502885389.firebaseio.com',
   panoptesAppId: '907392fdbc092096534ea6b3658e04ff5a74ac6110326b1c24f86d91cf80fb5e',
-  panoptesReturnUrl: 'http://local.zooniverse.org:3000',
+  panoptesReturnUrl: 'https://vote.zooniverse.org',
   storageBucket: 'project-6243802502502885389.appspot.com',
 };
 
